05475451c224ad958feee05b8f151cf674649507
Pepijn Mistiaen <pmistiaen@maxlinear.com>
2019-10-17 18:53:14 +0200

    Do not load device tracker

--- homeassistant/components/ddwrt/__init__.py
+++ homeassistant/components/ddwrt/__init__.py
@@ -77,8 +77,8 @@ async def async_setup(hass, config):
     #     )
     # )
 
-    hass.async_create_task(
-        async_load_platform(hass, "device_tracker", DOMAIN, {}, config)
-    )
+    #hass.async_create_task(
+    #    async_load_platform(hass, "device_tracker", DOMAIN, {}, config)
+    #)
 
     return True
